994ac12425bcf7cf54dd3f26a69d9b0b390a6a83
Oleksandr Tertyshnyk <aleksandr.tertyshnyk@gmail.com>
2019-07-20 22:53:45 +0300

    HT done

---
+++ dotnet-bank_bankomat_client/dotnet-bank_bankomat_client/Menu.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+
+struct MenuStruct {
+    public int No;
+    public string Title;
+    public Action action;
+}
+
+namespace dotnet_bank_bankomat_client
+{
+    public static class Menu
+    {
+        private static int NoCnt = 0;
+        public static Point Coord { get; set; }
+        public static string Titel { get; set; }
+        static List<MenuStruct> MenuList;
+
+        static int _MaxLen;
+        public static int MaxLen
+        {
+            get { return _MaxLen; }
+            set { if (_MaxLen < value) _MaxLen = value; }
+        }
+        static int _Select;
+        public static int Select
+        {
+            get { return _Select; }
+            set { if (0 <= _Select && _Select < MenuList.Count) _Select = value; }
+        }
+        public static ConsoleColor ColourTitle { get; set; }
+        public static ConsoleColor ColourMenu { get; set; }
+        public static ConsoleColor ColourSelect { get; set; }
+
+        public static void SetUp(string title, int x = 0, int y = 0)
+        {
+            Point t = new Point();
+            t.X = x;
+            t.Y = y;
+            Coord = t;
+            Titel = title;
+            MenuList = new List<MenuStruct>();
+            MaxLen = title.Length + 4;
+            Select = 0;
+            ColourTitle = ConsoleColor.Cyan;
+            ColourMenu = ConsoleColor.White;
+            ColourSelect = ConsoleColor.Green;
+        }
+
+        static private void Down()
+        {
+            Select = (Select + 1 < MenuList.Count) ? ++Select : 0;
+            DrawMenu();
+        }
+
+        static private void Up()
+        {
+            Select = (Select > 0) ? --Select : MenuList.Count - 1;
+            DrawMenu();
+        }
+
+        public static void Action(ConsoleKeyInfo pressKey)
+        {
+            if (pressKey.Key == ConsoleKey.UpArrow)
+            {
+                Up();
+                return;
+            }
+
+            if (pressKey.Key == ConsoleKey.DownArrow)
+            {
+                Down();
+                return;
+            }
+
+            if (pressKey.Key == ConsoleKey.Enter)
+            {
+                MenuList[Select].action();
+
+                Console.Clear();
+                DrawMenu();
+            }
+        }
+
+        public static void AddMenu(string Title, Action action)
+        {
+            ++NoCnt;
+            MenuStruct CurrentMenu = new MenuStruct();
+            CurrentMenu.No = NoCnt;
+            CurrentMenu.Title = Title;
+            CurrentMenu.action = action;
+
+            MenuList.Add(CurrentMenu);
+            MaxLen = (MaxLen < Title.Length + 6) ? Title.Length + 6 : MaxLen;
+        }
+
+        private static void DrawBlock(ConsoleColor c, int add = 0)
+        {
+            Console.ForegroundColor = c;
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            Console.SetCursorPosition(Coord.X, Coord.Y + add);
+            Console.Write("+");
+            for (int i = 0; i < MaxLen; i++)
+                Console.Write("-");
+            Console.Write("+");
+
+            Console.SetCursorPosition(Coord.X, Coord.Y + add + 1);
+            Console.Write("|");
+            for (int i = 0; i < MaxLen; i++)
+                Console.Write(" ");
+            Console.Write("|");
+
+            Console.SetCursorPosition(Coord.X, Coord.Y + add + 2);
+            Console.Write("+");
+            for (int i = 0; i < MaxLen; i++)
+                Console.Write("-");
+            Console.Write("+");
+        }
+
+        public static void DrawMenu()
+        {
+            Console.SetCursorPosition(Coord.X, Coord.Y);
+            Console.ForegroundColor = ColourTitle;
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            for (int i = 0; i < ((MaxLen - Titel.Length) / 2) + 1; ++i)
+                Console.Write(" ");
+            Console.WriteLine(Titel);
+            Console.SetCursorPosition(Coord.X, Coord.Y + 1);
+            for (int i = 0; i <= MaxLen + 1; ++i)
+                Console.Write("=");
+            Console.WriteLine();
+
+            Console.ForegroundColor = ColourMenu;
+            for (int i = 0; i < MenuList.Count; ++i)
+            {
+                if (i == Select)
+                {
+                    DrawBlock(ColourSelect, 2 + i * 3);
+                    Console.SetCursorPosition(Coord.X + 1, Coord.Y + 3 + i * 3);
+                    Console.ForegroundColor = ColourSelect;
+                    Console.Write(" * ");
+                    Console.Write(MenuList[i].Title);
+                    for (int j = 0; j < MaxLen - MenuList[i].Title.Length - 6; ++j)
+                        Console.Write(" ");
+                    Console.Write(" * ");
+                    Console.ForegroundColor = ColourMenu;
+                    continue;
+                }
+
+                DrawBlock(ConsoleColor.White, 2 + i * 3);
+                Console.SetCursorPosition(Coord.X + 1, Coord.Y + 3 + i * 3);
+                Console.Write("   ");
+                Console.WriteLine(MenuList[i].Title);
+                for (int j = 0; j < MaxLen - MenuList[i].Title.Length - 3; ++j)
+                    Console.Write(" ");
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public static void Clear()
+        {
+            Console.SetCursorPosition(Coord.X, Coord.Y);
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            int h = 2 + MenuList.Count * 3;
+
+            for (int i = 0; i < h; ++i)
+            {
+                Console.SetCursorPosition(Coord.X, Coord.Y + i);
+                for (int j = 0; j < MaxLen + 2; ++j)
+                {
+                    Console.Write(" ");
+                }
+            }
+
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+    }
+}
---
+++ metodichka 11 xml/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Xml;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+		//-- 6 --
+
+
+		/// <summary>
+		///The <c>Distance</c> class provides methods 
+		///for converting kilometers to and from miles.
+		/// </summary>
+		public class Distance
+		{
+			/// <summary>
+			///Converts kilometers to miles.
+			/// </summary>
+			/// <param name="kilometers">
+			///Used to indicate kilometers. 
+			///A <see cref="double"/>
+			///type representing a value.
+			/// </param>
+			/// <exception cref="ArgumentException">
+			///If <paramref name="kilometers"/>
+			///is negative.
+			/// </exception>
+			/// <returns>Returns the distance in miles.
+			/// </returns>
+			public static double KilometersToMiles(double kilometers)
+			{
+				if (kilometers < 0)
+				{
+					throw new ArgumentException("The value must be positive.");
+				}
+
+				return kilometers * 0.621371;
+			}
+
+			/// <summary>
+			///Converts miles to kilometers.
+			/// <param name="miles">
+			///Used to indicate miles. 
+			/// A <see cref="double"/>type representing a value.
+			/// </param>
+			/// <exception cref="ArgumentException">
+			///If <paramref name="miles"/>is negative.
+			/// </exception>
+			/// <returns>Returns the distance in 
+			///kilometers.</returns>
+			public static double MilesToKilometers(double miles)
+			{
+				if (miles < 0)
+				{
+					throw new ArgumentException("The value must be positive.");
+				}
+
+				return miles * 1.60934;
+			}
+		}
+
+			// -- 2	--
+
+			static void OutputNode(XmlNode node)
+		{
+			Console.WriteLine($"Type={node.NodeType}\tName={ node.Name}\tValue ={ node.Value}");
+
+			if (node.HasChildNodes)
+			{
+				foreach (XmlNode child in node.ChildNodes)
+					OutputNode(child);
+			}
+		}
+
+		static void Main(string[] args)
+        {
+			// -- 1 --
+
+			//XmlTextWriter writer = null;
+
+			//         try
+			//         {
+			//             writer = new XmlTextWriter("Cars.xml", System.Text.Encoding.Unicode);
+			//             writer.Formatting = Formatting.Indented;
+			//             writer.WriteStartDocument();
+			//             writer.WriteStartElement("Cars");
+			//             writer.WriteStartElement("Car");
+			//             writer.WriteAttributeString("Image", "MyCar.jpeg");
+			//             writer.WriteElementString("Manufactured", "La Hispano Suiza de Automovils");
+			//             writer.WriteElementString("Model", "Alfonso");
+			//             writer.WriteElementString("Year", "1912");
+			//             writer.WriteElementString("Color", "Black");
+			//             writer.WriteElementString("Speed", "130");
+			//             writer.WriteEndElement();
+			//             Console.WriteLine("The Cars.xml file is generated!");
+			//         }
+			//         catch (Exception ex)
+			//         {
+			//             Console.WriteLine(ex.Message);
+			//         }
+			//         finally
+			//         { 
+			//	if (writer != null) writer.Close();
+			//         }
+
+
+			//-- 2 --
+
+			//try
+			//{
+			//	XmlDocument doc = new XmlDocument();
+			//	doc.Load("Cars.xml");
+			//	OutputNode(doc.DocumentElement);
+			//}
+			//catch (Exception ex)
+			//{
+			//	Console.WriteLine(ex.Message);
+			//}
+
+
+
+			//-- 3 --
+
+			//try
+			//{
+			//	XmlDocument doc = new XmlDocument();
+			//	doc.Load("Cars.xml");
+			//	XmlNode root = doc.DocumentElement; 
+
+			//	// удалить первый элемент Cars
+			//	root.RemoveChild(root.FirstChild); 
+
+			//	// создать узлы элементов.
+			//	XmlNode bike = doc.CreateElement("Motorcycle");
+			//	XmlNode elem1 = doc.CreateElement("Nanufactured");
+			//	XmlNode elem2 = doc.CreateElement("Model");
+			//	XmlNode elem3 = doc.CreateElement("Year");
+			//	XmlNode elem4 = doc.CreateElement("Color");
+			//	XmlNode elem5 = doc.CreateElement("Engine"); 
+
+			//	// создать текстовые узлы
+			//	XmlNode text1 = doc.CreateTextNode("Harley-Davidson Motor Co.Inc.");
+			//	XmlNode text2 = doc.CreateTextNode("Harley 20J");
+			//	XmlNode text3 = doc.CreateTextNode("1920");
+			//	XmlNode text4 = doc.CreateTextNode("Olive");
+			//	XmlNode text5 = doc.CreateTextNode("37 HP");
+
+			//	// присоединить текстовые узлы к узлам элементов
+			//	elem1.AppendChild(text1);
+			//	elem2.AppendChild(text2);
+			//	elem3.AppendChild(text3);
+			//	elem4.AppendChild(text4);
+			//	elem5.AppendChild(text5);
+
+			//	// присоединить узлы элементов к узлу bike
+			//	bike.AppendChild(elem1);
+			//	bike.AppendChild(elem2);
+			//	bike.AppendChild(elem3);
+			//	bike.AppendChild(elem4);
+			//	bike.AppendChild(elem5);
+
+			//	// присоединить узел bike к корневому узлу
+			//	root.AppendChild(bike);
+
+			//	// сохранить измененный документ
+			//	doc.Save("Motorcycle.xml"); 
+			//	Console.WriteLine("The Motorcycle.xml file is generated!");
+			//}
+			//catch (Exception ex)
+			//{
+			//	Console.WriteLine(ex.Message);
+			//}
+
+
+			//-- 4 --
+
+			//XmlTextReader reader = null;
+
+			//try
+			//{
+			//	reader = new XmlTextReader("Cars.xml");
+			//	reader.WhitespaceHandling = WhitespaceHandling.None;
+
+			//	while (reader.Read())
+			//	{
+			//		Console.WriteLine($"Type={reader.NodeType}\t\tName={ reader.Name}\t\tValue={ reader.Value}");
+
+			//		if (reader.AttributeCount > 0)
+			//		{
+			//			while (reader.MoveToNextAttribute())
+			//			{
+			//				Console.WriteLine($"Type={reader.NodeType}\tName={reader.Name}\tValue={reader.Value}");
+			//			}
+			//		}
+			//	}
+			//}
+			//catch(Exception ex)
+			//{
+			//	Console.WriteLine(ex.Message);
+			//}
+			//finally
+			//{
+			//	if(reader != null) reader.Close();
+			//}
+
+			//-- 5 --
+
+			XmlTextReader reader = null;
+			try
+			{
+				reader = new XmlTextReader("Cars.xml");
+				reader.WhitespaceHandling = WhitespaceHandling.None;
+
+				while (reader.Read())
+				{
+					if (reader.NodeType == XmlNodeType.Element && reader.Name == "Car" && reader.AttributeCount > 0)
+					{
+						while (reader.MoveToNextAttribute())
+						{
+							if (reader.Name == "Image")
+							{
+								Console.WriteLine(reader.Value);
+								break;
+							}
+						}
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+			finally
+			{
+				if (reader != null) reader.Close();
+			}
+
+
+			
+
+			Console.Read();
+        }
+
+
+	}
+}
